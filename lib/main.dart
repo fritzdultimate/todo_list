363b80a4e24e4f201846bd092e56762a8fe09cb0
fritzdultimate <fritzdultimate@gmail.com>
2020-12-21 22:10:15 +0100

    path_provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'package:path_provider/path_provider.dart';
 
 import 'package:flutter/material.dart';
 
@@ -7,17 +8,19 @@ import 'features/ui/pages/todo_lists.dart';
 import 'package:hive/hive.dart';
 
 void main() {
-  var path = Directory.current.path;
-  Hive.init(path);
+  // var path = Directory.current.path;
+  // Hive.init(path);
   runApp(MyApp());
 }
    
   class MyApp extends StatelessWidget {
+    var path = Directory.current.path;
     @override
     Widget build(BuildContext context) {
       return MaterialApp(
         title: 'Todo List',
         home: TodoLists()
+
       );
     }
   }
